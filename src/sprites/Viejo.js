94add512835b4a195739d3114bde6b2b2b8d1b57
Daniel Espinosa <ec.dani@gmail.com>
2017-12-08 01:15:19 +0100

    Viejo que da volteretas

--- src/sprites/Viejo.js
+++ src/sprites/Viejo.js
@@ -3,11 +3,93 @@ import { Sprite } from 'phaser'
 class Viejo extends Sprite {
   constructor(game, x, y) {
     super(game, x, y, 'rpg-maker-sprites', 54)
-
+    this.game = game
     game.add.existing(this)
-    game.physics.p2.enable(this)
+    game.physics.arcade.enable(this)
     this.body.velocity.x = 10
+
+
+    this.desplazamiento(6) // Del 0 al 7
+
+    this.cursors = game.input.keyboard.createCursorKeys();
+    //  Creates 30 bullets, using the 'bullet' graphic
+    this.weapon = this.game.add.weapon(30, 'bullet');
+    //  The bullet will be automatically killed when it leaves the world bounds
+    this.weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+          
+     //  The speed at which the bullet is fired
+    this.weapon.bulletSpeed = 600;
+          
+    //  Speed-up the rate of fire, allowing them to shoot 1 bullet every 60ms
+    this.weapon.fireRate = 100;
+    this.weapon.trackSprite(this, 0, 0, true);
+          
+    this.fireButton = this.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
   }
+
+  desplazamiento(n) {
+    if (n>3) {
+      /*this.animations.add('down', [54, 55, 56], 10, true)
+      this.animations.add('right', [78, 79, 80], 10, true)
+      this.animations.add('left', [66, 67, 68], 10, true)
+      this.animations.add('up', [90, 91, 92], 10, true)*/
+      n = n-4
+      this.animations.add('down', [3*n* +4*12,       3*n +1+48,       3*n +2+48], 10, true)
+
+      this.animations.add('down', [3*n +48,       3*n +1+48,       3*n +2+48], 10, true)
+      this.animations.add('left', [3*n +12+48,   3*n +1 +12+48,   3*n +2 +12+48], 10, true)
+      this.animations.add('right',[3*n +12*2+48, 3*n +1 +12*2+48, 3*n +2 +12*2+48], 10, true)
+      this.animations.add('up',   [3*n +12*3+48, 3*n +1 +12*3+48, 3*n +2 +12*3+48], 10, true)
+    } else {
+      this.animations.add('down', [3*n,       3*n +1,       3*n +2], 10, true)
+      this.animations.add('left', [3*n +12,   3*n +1 +12,   3*n +2 +12], 10, true)
+      this.animations.add('right',[3*n +12*2, 3*n +1 +12*2, 3*n +2 +12*2], 10, true)
+      this.animations.add('up',   [3*n +12*3, 3*n +1 +12*3, 3*n +2 +12*3], 10, true)
+    }
+  }
+
+  update() {
+    
+        if (this.cursors.up.isDown)
+        {
+          //this.game.physics.arcade.accelerationFromRotation(this.rotation, 300, this.body.acceleration);
+          this.body.velocity.y = -150
+          this.animations.play('up')
+        }
+        else
+        {
+          this.body.acceleration.set(0);
+        }
+    
+        if (this.cursors.left.isDown)
+        {
+          //this.body.angularVelocity = -300;
+          this.body.velocity.x = -150
+          this.animations.play('left')
+        }
+        else if (this.cursors.right.isDown)
+        {
+          //this.body.angularVelocity = 300;
+          this.body.velocity.x = 150
+          this.animations.play('right')
+        }
+        else
+        {
+          //this.body.angularVelocity = 0;
+          this.body.velocity.x = 0
+          this.body.velocity.y = 0
+        }
+    
+        if (this.fireButton.isDown)
+        {
+          this.weapon.fire();
+          this.body.angularVelocity = -300;
+        }
+    
+        this.game.world.wrap(this, 16);
+    
+    }
+
 }
 
 export default Viejo
--- src/states/Game.js
+++ src/states/Game.js
@@ -19,6 +19,8 @@ export default class extends Phaser.State {
     banner.smoothed = false
     banner.anchor.setTo(0.5)
 
+
+
     this.game.physics.startSystem(Phaser.Physics.P2JS)
 
     this.mushroom = new Mushroom({
@@ -33,18 +35,9 @@ export default class extends Phaser.State {
 
  //WEAPON
 
-      //  Creates 30 bullets, using the 'bullet' graphic
-      this.weapon = this.game.add.weapon(30, 'bullet');
+
   
-      //  The bullet will be automatically killed when it leaves the world bounds
-      this.weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
-  
-      //  The speed at which the bullet is fired
-      this.weapon.bulletSpeed = 600;
-  
-      //  Speed-up the rate of fire, allowing them to shoot 1 bullet every 60ms
-      this.weapon.fireRate = 100;
-  
+
       //sprite = this.add.sprite(400, 300, 'ship');
   
       //sprite.anchor.set(0.5);
@@ -57,49 +50,19 @@ export default class extends Phaser.State {
       //  Tell the this.weapon to track the 'player' Sprite
       //  With no offsets from the position
       //  But the 'true' argument tells the this.weapon to track sprite rotation
-      this.weapon.trackSprite(this.mushroom, 0, 0, true);
+
   
-      this.cursors = this.input.keyboard.createCursorKeys();
+
   
-      this.fireButton = this.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR, () => console.log("hey"));
+     
 
 
     this.viejo = new Viejo(game, 200, 200)
+     new Viejo(game, 100, 200)
 
   }
 
-  update() {
-    
-        if (this.cursors.up.isDown)
-        {
-          this.game.physics.arcade.accelerationFromRotation(this.mushroom.rotation, 300, this.mushroom.body.acceleration);
-        }
-        else
-        {
-          this.mushroom.body.acceleration.set(0);
-        }
-    
-        if (this.cursors.left.isDown)
-        {
-          this.mushroom.body.angularVelocity = -300;
-        }
-        else if (this.cursors.right.isDown)
-        {
-          this.mushroom.body.angularVelocity = 300;
-        }
-        else
-        {
-          this.mushroom.body.angularVelocity = 0;
-        }
-    
-        if (this.fireButton.isDown)
-        {
-          this.weapon.fire();
-        }
-    
-        this.game.world.wrap(this.mushroom, 16);
-    
-    }
+
 
   render () {
     if (__DEV__) {
